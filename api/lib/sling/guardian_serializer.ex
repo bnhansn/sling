c177d634d235734688039ef93f2f33735c0419e4
Ben Hansen <bnhansn@gmail.com>
2016-10-21 10:52:24 -0400

    Forgot to commit guardian_serializer.ex

---
+++ api/lib/sling/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule Sling.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias Sling.Repo
+  alias Sling.User
+
+  def for_token(user = %User{}), do: {:ok, "User:#{user.id}"}
+  def for_token(_), do: {:error, "Unknown resource type"}
+
+  def from_token("User:" <> id), do: {:ok, Repo.get(User, String.to_integer(id))}
+  def from_token(_), do: {:error, "Unknown resource type"}
+end
