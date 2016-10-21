c69cfaf528efb63c3279bd5c0317d10554dd5a94
Ben Hansen <bnhansn@gmail.com>
2016-10-21 09:20:59 -0400

    Add bootstrap, font-awesome, base styles

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './containers/App';
+import './styles/bootstrap.css';
+import './styles/font-awesome.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <Provider store={store}>
