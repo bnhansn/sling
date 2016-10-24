61e929ad14dac0910528f5086e7cc4408dd8fe0a
Ben Hansen <bnhansn@gmail.com>
2016-10-24 09:45:11 -0400

    Add default login credentials

--- web/src/containers/Login/index.js
+++ web/src/containers/Login/index.js
@@ -22,7 +22,10 @@ class Login extends Component {
     return (
       <div style={{ flex: '1' }}>
         <Navbar />
-        <LoginForm onSubmit={this.handleLogin} />
+        <LoginForm
+          onSubmit={this.handleLogin}
+          initialValues={{ email: 'anon@test.com', password: 'password' }}
+        />
       </div>
     );
   }
