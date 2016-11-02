fcf28370373e3c9e7a84138162ddc69abe73c5e3
Ben Hansen <bnhansn@gmail.com>
2016-11-02 04:54:01 -0400

    Revert ssl troubleshooting

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :sling, Sling.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "http", host: "nameless-reaches-32969.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "nameless-reaches-32969.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
